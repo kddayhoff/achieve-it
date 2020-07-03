709925152f0cc85bd8f1d8b285b0d2a085b1e1d8
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-07-02 20:29:01 -0400

    add API.js to add axios for calendar add

---
+++ client/src/utils/API.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+
+const axios = require("axios");
+
+axios({
+    "method":"POST",
+    "url":"https://cronofyraygorodskijv1.p.rapidapi.com/createCalendar",
+    "headers":{
+    "content-type":"application/x-www-form-urlencoded",
+    "x-rapidapi-host":"CronofyraygorodskijV1.p.rapidapi.com",
+    "x-rapidapi-key":"af08c17399msh3836ab03df986a3p172df3jsnc36e280301ab",
+    "useQueryString":true
+    },"data":{
+    "profileId":"1",
+    "accessToken":"!]#123@abczyx`+,-?876{}pon.[%()0",
+    "name":"test"
+    }
+    })
+    .then((response)=>{
+      console.log(response)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+

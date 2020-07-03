2cb7ac155ff1723474c1178bed4b48b266e6e7af
Annaruth McBride <armcbride17@gmail.com>
2020-07-03 16:29:20 -0500

    added Container component

---
+++ client/src/components/Container.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+export default function Container() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+      <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
+  );
+}

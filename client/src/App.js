107c1801d3b57f7550c3a695cff15b02c6e44be3
Annaruth McBride <armcbride17@gmail.com>
2020-07-02 19:32:46 -0500

    added Navbar and Footer layouts to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import Calendar from 'react-calendar';
-
+import Footer from "./components/Layouts/Footer";
+import Navbar from "./components/Layouts/Navbar";
 
 function App(){
         return(
+            <>
+            <Navbar/>
             <Calendar/>
+            <Footer/>
+            </>
         );
     
 }
---
+++ client/src/components/Layouts/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+function Footer() {
+  return (
+    <>
+    <Button variant="contained" color="primary">I'm a Footer</Button>
+    </>
+  );
+}
+
+export default Footer;

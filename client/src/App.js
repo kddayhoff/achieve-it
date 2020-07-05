3de6eacdc2a8c6aaa62691fd7766ee4758770ac0
Annaruth McBride <armcbride17@gmail.com>
2020-07-05 14:09:00 -0500

    removed redundancies

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
 import React, {Component} from 'react';
-import Calendar from 'react-calendar';
-// import Footer from "./components/Layouts/Footer";
-// import Navbar from "./components/Layouts/Navbar";
 import Container from "./components/Container";
 
 function App(){

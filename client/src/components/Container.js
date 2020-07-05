6a5293d590886136fe8376bfe814259e806e91e8
Annaruth McBride <armcbride17@gmail.com>
2020-07-05 14:08:03 -0500

    imported components into container and brought that into App.

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -2,6 +2,9 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Footer from "./Layouts/Footer";
+import Navbar from "./Layouts/Navbar";
+import Calendar from 'react-calendar';
 
 // We may not need to use this, but for this whole page container, we would bring in everything packaged together here and then import this Container into the App.js file.
 
@@ -9,9 +12,14 @@ export default function PageContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
+      <Container maxWidth="lg">
       {/* <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} /> */}
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+
+      <Navbar/>
+            <Calendar/>
+            <Footer/>
+
+        <Typography component="div" style={{ backgroundColor: '#3f51b5', height: '100vh' }} />
       </Container>
     </React.Fragment>
   );

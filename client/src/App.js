9a3fcd42b7dc023358b0c103ffbf910dc718cd88
Annaruth McBride <armcbride17@gmail.com>
2020-07-05 16:43:31 -0500

    adding login cred to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import PageContainer from "./components/Container";
 
 function App(){
-        return(
-            <>
-            
+    const [registerUsername, setRegisterUsername] = useState('');
+    const [registerPassword, setRegisterPassword] = useState('');
+    const [loginUsername, setLoginUsername] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+
+         return(
+            <div className= "App">
+            <div>
+            <h1>Register</h1>
+            <input placeholder="username" onChange={e => setRegisterUsername(e.target.value)}/>
+            <input placeholder="password" onChange={e => setRegisterPassword(e.target.value)}/>
+            </div>
+            <div>
+            <h1>Login</h1>
+            <input placeholder="username" onChange={e => setLoginUsername(e.target.value)}/>
+            <input placeholder="password" onChange={e => setLoginPassword(e.target.value)}/>
+            </div>
+
+            <div>
+                <h1>Get User</h1>
+                <button>Submit </button>
+            </div>
             <PageContainer/>
             
-            </>
+            </div>
         );
     
 }

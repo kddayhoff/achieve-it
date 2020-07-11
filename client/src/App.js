888b1167142863f3fb6e6187aee6a53d2b0bb53b
Annaruth McBride <armcbride17@gmail.com>
2020-07-11 09:42:37 -0500

    added a wrapper and took out the typography

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Footer from "./components/Layouts/Footer";
 import Navbar from "./components/Layouts/Navbar";
-// import CalApp from './components/Calendar';
+import Wrapper from './components/Wrapper';
 import Dashboard from "./components/Pages/Dashboard";
 import Signup from "./components/Pages/Signup";
 import { HashRouter as Router, Route} from "react-router-dom";
@@ -16,17 +15,21 @@ function App(){
              <>
             <div className= "App">
            <React.Fragment>
+           <Router>
+               <Wrapper>
       <CssBaseline />
       <Container maxWidth="lg">
-      <Router>
+      
       <Navbar/>
       <Route exact path="/" component={Dashboard} />
       <Route exact path="/signup" component={Signup} />
       {/* <CalApp/> */}
       <Footer/>
-      </Router>
-        <Typography component="div" style={{ backgroundColor: '#3f51b5', height: '100vh' }} />
+      
+    
       </Container>
+      </Wrapper>
+      </Router>
     </React.Fragment>
             </div>
             </>

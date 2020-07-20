b6e1a39c39dc3b8f7bc5e5e5df174113a28a4b47
Annaruth McBride <armcbride17@gmail.com>
2020-07-20 14:29:43 -0500

    adjust

--- client/src/components/Layouts/Navbar.js
+++ client/src/components/Layouts/Navbar.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
-import Axios from 'axios';
+// import Axios from 'axios';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,

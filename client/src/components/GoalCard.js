e87d7e775bbf2f6d0e63842a973b6b113c518fc8
Annaruth McBride <armcbride17@gmail.com>
2020-07-21 20:37:25 -0500

    updates, fleshing props in Goal Card and Content

--- client/src/components/GoalCard.js
+++ client/src/components/GoalCard.js
@@ -1,10 +1,17 @@
-//prop.goals
+import React from 'react';
 
-class GoalCard extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+function GoalCard(props){
+    console.log(props);
+
+    return(
+<Paper className={classes.paper} key={props._id}>
+                  {props.goal} -
+                  {props.task}
+                  {props.date}
+                </Paper>
+
+    )
+    
 }
- 
+
 export default GoalCard;
---
+++ client/src/components/GoalCardContent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import GoalCard from './GoalCard';
+
+class GoalCard extends Component {
+
+
+    state = {  }
+    render() { 
+
+
+
+        return (
+            <>
+
+
+            </>
+          );
+    }
+}
+ 
+export default GoalCard;

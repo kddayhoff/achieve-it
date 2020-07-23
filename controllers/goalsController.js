35ddd7648bcaf2a2b244db375064765a2d87c7ff
Karla Dayhoff <kddayhoff@gmail.com>
2020-07-22 20:34:26 -0500

    client's ID now kept active while in session -- reordered server side code

--- controllers/goalsController.js
+++ controllers/goalsController.js
@@ -1,36 +1,39 @@
 const db = require("../models");
 
 module.exports = {
- 
   //creates new goal with associated task and pushes it to a specific user that is logged in
- create: (req, res) => {
-    db.Goal
-      .create(req.body)
-      .then(({_id}) => 
-        
-      db.User.findByIdAndUpdate(req.params.id, 
-        { $push: {goals: _id}}, {new: true}) 
-      .then(dbModel => {
+
+  create: (req, res) => {
+    console.log(req.body);
+    console.log(req.user);
+    db.Goal.create(req.body)
+      .then(({ _id }) => {
+        db.User.findByIdAndUpdate(
+          { _id: req.user._id },
+          { $push: { goals: _id } },
+          { new: true }
+        )
+      })
+      .then((dbModel) => {
+        console.log("dbModel:", dbModel);
         res.json(dbModel);
-      })) 
-      .catch(err => res.status(422).json(err));
+      })
+      .catch((err) => res.json(err.body));
   },
 
-  //finds a specific goal for a user to update 
+  //finds a specific goal for a user to update
   update: (req, res) => {
-    db.Goal
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    db.Goal.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
 
- //finds a specific goal that the user can delete
+  //finds a specific goal that the user can delete
   delete: (req, res) => {
-    db.Goal
-      .findByIdAndDelete(req.params.id )
-     
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }, 
-}
+    db.Goal.findByIdAndDelete(req.params.id)
+
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
 

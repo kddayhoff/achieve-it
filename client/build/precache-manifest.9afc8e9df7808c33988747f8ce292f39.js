9507b069b8db101cfc8c47c212033b511b7b7423
Alec Down <alecdown@gmail.com>
2020-07-25 17:18:27 -0500

    fixes form ref causing .split error

---
+++ client/build/precache-manifest.9afc8e9df7808c33988747f8ce292f39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8deca83765e086c747d0d24ae243c97f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d515eb8a478156209b33",
+    "url": "/static/css/2.b6ddee0d.chunk.css"
+  },
+  {
+    "revision": "429ddb9eb1bb1f372a6b",
+    "url": "/static/css/main.9cd797d7.chunk.css"
+  },
+  {
+    "revision": "d515eb8a478156209b33",
+    "url": "/static/js/2.1fee265b.chunk.js"
+  },
+  {
+    "revision": "3dfeb62e21bcc1fd7d19c0bcd56acb2f",
+    "url": "/static/js/2.1fee265b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429ddb9eb1bb1f372a6b",
+    "url": "/static/js/main.cd5ff6a8.chunk.js"
+  },
+  {
+    "revision": "bcb0cbec75a7e7bc05f9",
+    "url": "/static/js/runtime-main.4cc5e9eb.js"
+  }
+]);

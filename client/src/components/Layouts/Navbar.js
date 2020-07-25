d04645e2b800fd1eadebfb23f38e2963209b7ade
Karla Dayhoff <kddayhoff@gmail.com>
2020-07-24 20:50:08 -0500

    updated readme to include app screenshots

--- client/src/components/Layouts/Navbar.js
+++ client/src/components/Layouts/Navbar.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
-// import Axios from 'axios';
+import Axios from 'axios';
+import { Redirect} from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -30,24 +31,28 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-// const getUser = () => {
-//   Axios({
-//         method: "GET",
-//         withCredentials: true,
-//         url:"/user"
-//      }).then((res) => {
-//         //  setData(res.data);
-//          console.log(res.data)
-//    });
-//  };
 
 export default function Navbar() {
   const classes = useStyles();
   // const user = getUser();
 
+  const [loggedOut, setLoggedOut] = useState(false); 
+
+const logout = () => {
+  Axios({
+        method: "GET",
+        withCredentials: true,
+        url:"/logout"
+     }).then((res) => {
+        //  setData(res.data);
+        setLoggedOut(true)
+     })}
+ 
+
     
   return (
     <div className={classes.root}>
+      {loggedOut ? <Redirect to={{pathname:"/login"}}/> : null}
       <AppBar className={classes.nav} position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
@@ -56,7 +61,7 @@ export default function Navbar() {
           <Link style={{ textDecoration: 'none' }} to="/">
           <Button className={classes.btns}>Log In</Button></Link>
           <Link style={{ textDecoration: 'none' }} to="/logout">
-          <Button className={classes.btns}>Log Out</Button></Link>
+          <Button className={classes.btns} onClick={logout}>Log Out</Button></Link>
         </Toolbar>
       </AppBar>
     </div>
